58b6dab4171b83d3d64e50078c89977567cddd1c
anniepineda <anniepineda@outlook.com>
2019-11-07 22:57:13 -0800

    update
    git add.
    
    git add.
    git add .
    ls
    ls

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 
-alert('Hi, welcome! Lets play a guessing game so you can learn more about me! Please answer the following questions with a yes or no answer');
+var user = prompt('Hi, welcome! What is your name?')
+console.log('user name is' + user);
+
+alert('Hi there ' + user + ' so you can learn more about me lets play a guessing game! Please answer the following questions with a yes or no answer.');
 
 // 5 questions for user
 var states = prompt('Have I lived in more than three states?');
@@ -26,14 +29,14 @@ if (hobby.toLowerCase()=== "no" ){
 } 
 
 var university = prompt( 'Did I graduate form Kansas State University?');
-console.log('yes or no:', universituy);
+console.log('yes or no:', university);
 
 if (university.toLowerCase()=== "yes"){
     alert("Yes, you are correct. I graduated from her batchelors from K-State.");
 }
 
-if (states.toLowerCase()=== "no" ){
-    alert("Sorry, you are incorecct. Annie has lived in California, Kansas, Georgia and Washington.");
+if (university.toLowerCase()=== "no" ){
+    alert("Sorry, you are incorecct. I graduated from Kansas State in 2015.");
 } 
 
 var french = prompt('Is Annie learning French?');
@@ -47,13 +50,13 @@ if (french.toLowerCase()=== "no" ){
     alert("Sorry, you are incorecct. For the past two years I have been learning french.");
 } 
 
-var volunteer = promp('Does Annie like to volunteer?');
+var volunteer = prompt('Does Annie like to volunteer?');
 console.log('yes or no', volunteer);
 
-if (voulenteer.toLowerCase()=== "yes"){
-    alert("Yes, you are correct. I have voulenteered in different organizations since I was 15.");
+if (volunteer.toLowerCase()=== "yes"){
+    alert("Yes, you are correct. I have volunteered in different organizations since I was 15.");
 }
 
-if (voulenteer.toLowerCase()=== "no" ){
-    alert("Sorry, you are incorecct. I have voulenteered in different organizations since I was 15.");
+if (volunteer.toLowerCase()=== "no" ){
+    alert("Sorry, you are incorecct. I have volunteered in different organizations since I was 15.");
 } 
